68af311731c39b2164e105c17445c9242c79649e
pandey-rakshit <pandeyrakshit03@gmail.com>
2023-05-18 15:24:46 +0530

    ADD: GET and POST apis for fetch and create cats

--- src/cats/controllers/cat.ts
+++ src/cats/controllers/cat.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CatsService } from '../services';
+import { Cat } from '../interfaces';
+import { CreateCatDto } from '../dtos';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly service: CatsService) {}
+
+  @Get('')
+  findAll(): Promise<Cat[]> {
+    return this.service.findAll();
+  }
+
+  @Post()
+  create(@Body() CreateCatDto: CreateCatDto): void {
+    this.service.create(CreateCatDto);
+  }
 }
--- src/cats/services/cat.ts
+++ src/cats/services/cat.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Cat } from '../interfaces';
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  private readonly cats: Cat[] = [];
+
+  create(cat: Cat): void {
+    this.cats.push(cat);
+  }
+
+  async findAll(): Promise<Cat[]> {
+    return this.cats;
+  }
+}
